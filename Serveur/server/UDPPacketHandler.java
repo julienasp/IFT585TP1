8f0fe9d81e70a975c23c58288ba6bff20d83dcee
julienasp <julien.aspirot@usherbrooke.ca>
2016-01-27 19:59:12 -0500

    MAJ

--- Serveur/server/UDPPacketHandler.java
+++ Serveur/server/UDPPacketHandler.java
@@ -38,143 +38,21 @@ public class UDPPacketHandler implements Runnable {
 		UDPPacket udpPacket = (UDPPacket) Marshallizer.unmarshall(packetReceive);
 		logger.info("udpPacket receive " + String.valueOf(udpPacket.getType()));	
 		if (udpPacket.isForDownload()) {
-			//Setup the downloadHandler
-			
-			Reply reply = buildResponse((Request)message);
-			respond(reply);
+			//Setup the transmissionThread
+			Thread transmissionThread = new Thread(new transmissionHandler(udpPacket));
+                        transmissionThread.start();
+			logger.info("TransmissionHandler started");
 		}
                 else{
+                    //Setup the receptionHandlerThread
+			Thread receptionHandlerThread = new Thread(new receptionHandler(udpPacket));
+                        receptionHandlerThread.start();
+			logger.info("ReceptionHandler started");
                 }
-		logger.info("reply done");
+		
 	}
 	
-		//Answer to the request
-		private Reply buildResponse(Request request) {
-			Reply reply = new Reply(packetReceive.getAddress(),packetReceive.getPort(),request.getNumero());
-			switch(request.getMethode()) {
-			case list :
-				//old version with every detail
-				//Match[] listMatch = data.getAllMatch();
-				//reply.setValue(listMatch);	
-				
-				//new version
-				ListMatchName listMatch = data.getAllMatchName();
-				reply.setValue(listMatch);	
-				
-				break;
-				
-			case detail:
-				Object[] arguments = request.getArgument();
-				int matchID = (int) arguments[0];
-				logger.info("detail received with param : "+ String.valueOf(matchID));
-				Match matchDetail = data.getMatch(matchID);
-				reply.setValue(matchDetail);			
-				break;
-                        case betInfo:
-                            
-                                //We fetch the arguments
-                                Object[] args = request.getArgument();                
-                                int betMatchID = (int) args [0]; // The matchID of the request                            
-                                String betID = (String) args[1]; // The betID of the request
-                                
-                                logger.info("detail received with params : "+ String.valueOf(betMatchID + " and the betID: " + betID.toString() ));  
-                                
-                                //Respond object
-                                BetRespond betRespond = null;
-                                
-                                try {
-                                    //We get the total betting amount for the current match
-                                    float matchTotalBettingAmount = BetHandler.getTotalBettingAmount(betMatchID);
-                                    
-                                    logger.info("Total betting amount for the match #"+ String.valueOf(betMatchID + " is : " + String.valueOf(matchTotalBettingAmount) + "$" ));
-                                    
-                                    //We retreive the data of the Match
-                                    Match betMatchDetail = data.getMatch(betMatchID);
-                                    
-                                    if(betMatchDetail.getWinner() != null){
-
-                                        //We get the winnerTable that contains all the Bet object that won for that match.
-                                        Hashtable<String, Bet> winnerTable = BetHandler.getWinnerTable(betMatchDetail);
-
-                                        //If the current betID (from the request) is inside the winnerTable we proceed
-                                        if(winnerTable.containsKey(betID)){
-
-                                            //We set currentBet to the resquested bet with the betID 
-                                            Bet currentBet = winnerTable.get(betID);
-
-                                            logger.info("The current bet amount for the match #"+ String.valueOf(betMatchID + " is : " + String.valueOf(currentBet) + "$" ));
-
-                                            float currentBetAmount = currentBet.getBetAmount(); //contains the resquested bet dollar amount 
-
-                                            float totalWinningBetAmount = 0; //contains the sum of all the bet
-
-                                            //This iterator will go through the winnerTable 
-                                            Iterator<Map.Entry<String, Bet>> it = winnerTable.entrySet().iterator();
-
-                                            while (it.hasNext()) {
-                                              //We get the current entry of the winnerTable  
-                                              Map.Entry<String, Bet> entry = it.next();
-
-                                              //We get the value of that entry
-                                              Bet b = entry.getValue();
-
-                                              //We sum the Bet value to the existing totalWinningBetAmount
-                                              totalWinningBetAmount += b.getBetAmount();
-                                            }
-
-                                            logger.info("The total of the winning bet made for the match #"+ String.valueOf(betMatchID + " is : " + String.valueOf(totalWinningBetAmount) + "$" ));
-
-                                            float wonAmount = (float)( (currentBetAmount / totalWinningBetAmount) * (matchTotalBettingAmount * 0.75) );
-
-                                            betRespond = new BetRespond(betID, betMatchID, 1, currentBetAmount, wonAmount ); // won the bet
-                                            reply.setValue(betRespond);
-                                            
-                                            logger.info("The bet was a winning bet, and we reply.setValue contains a betRespond Object" );
-
-                                        }
-                                        else{
-                                            betRespond = new BetRespond(betID, betMatchID, 0 ); // Did not win the bet
-                                            reply.setValue(betRespond);
-                                            
-                                            logger.info("The bet was not a winning bet, and we reply.setValue contains a betRespond Object" );
-                                        }
-                                    }
-                                    else{
-                                        betRespond = new BetRespond(betID, betMatchID, 2 ); // Match isn't finish yet
-                                        reply.setValue(betRespond);
-                                        
-                                        logger.info("The match isn't finish yet for that match" );
-                                    }
-                                } catch (IOException ex) {
-                                        java.util.logging.Logger.getLogger(UDPPacketHandler.class.getName()).log(Level.SEVERE, null, ex);
-                                }
-                                
-                                
-				
-			default:
-				reply.setValue(new MessageError(MessageError.METHODEERROR)); 
-				break;
-			}								
-			logger.info("Message reply crafted");		
-			return reply;
-		}
-
-	protected void respond(Reply message) {
-		try {
-			//Message.getData()
-			logger.debug(message.toString());
-			byte[] reply = Marshallizer.marshallize(message);
-			DatagramPacket datagram = new DatagramPacket(reply,
-					reply.length, 
-					message.getDestination(),
-					message.getDestinationPort());
-			serverSocket.send(datagram); // émission non-bloquante
-		} catch (SocketException e) {
-			System.out.println("Socket: " + e.getMessage());
-		} catch (IOException e) {
-			System.out.println("IO: " + e.getMessage());
-		}
-	}
+		
 
 
 }
--- Serveur/server/receptionHandler.java
+++ Serveur/server/receptionHandler.java
@@ -6,6 +6,10 @@
 package server;
 
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import java.net.DatagramPacket;
@@ -17,6 +21,8 @@ import org.apache.log4j.Logger;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.Level;
 import protocole.UDPPacket;
 import utils.Marshallizer;
@@ -25,5 +31,204 @@ import utils.Marshallizer;
  * @author JUASP-G73-Android
  */
 public class receptionHandler implements Runnable{
+     private UDPPacket connectionPacket;
+    private DatagramSocket connectionSocket = null;
+    private DatagramPacket packetReceive;
+    private int seq = 0;
+    private int ack = 0;
+    private int fin = 0;
+    private Hashtable<Integer, UDPPacket> fenetre = new Hashtable<Integer,UDPPacket>();
+    private File theFile = new File("hd.jpg"); // Static, nous allons toujours utlisé le même fichier pour la transmission
     
+    private static final Logger logger = Logger.getLogger(transmissionHandler.class);
+    
+    /*************************************************************/
+    /********************   CONSTRUCTOR   ************************/
+    /*************************************************************/
+    
+    public receptionHandler(UDPPacket connectionPacket) {
+        this.connectionPacket = connectionPacket;
+    }
+
+    /*************************************************************/
+    /*****************   GETTER AND SETTER   *********************/
+    /*************************************************************/
+    
+    public DatagramSocket getConnectionSocket() {
+        return connectionSocket;
+    }
+
+    public void setConnectionSocket(DatagramSocket connectionSocket) {
+        this.connectionSocket = connectionSocket;
+    }
+
+    public DatagramPacket getPacketReceive() {
+        return packetReceive;
+    }
+
+    public void setPacketReceive(DatagramPacket packetReceive) {
+        this.packetReceive = packetReceive;
+    }
+
+    public int getSeq() {
+        return seq;
+    }
+
+    public void setSeq(int seq) {
+        this.seq = seq;
+    }
+
+    public int getAck() {
+        return ack;
+    }
+
+    public void setAck(int ack) {
+        this.ack = ack;
+    }
+
+    public int getFin() {
+        return fin;
+    }
+
+    public void setFin(int fin) {
+        this.fin = fin;
+    }
+
+    public Hashtable<Integer, UDPPacket> getFenetre() {
+        return fenetre;
+    }
+
+    public void setFenetre(Hashtable<Integer, UDPPacket> fenetre) {
+        this.fenetre = fenetre;
+    }
+
+    public File getTheFile() {
+        return theFile;
+    }
+
+    public void setTheFile(File theFile) {
+        this.theFile = theFile;
+    }
+
+    
+    /*************************************************************/
+    /*****************        METHODS        *********************/
+    /*************************************************************/
+    //Prépare les objets pour l'envoi
+    private void prepWindow(){       
+        try {            
+            BufferedInputStream bis; 
+            bis = new BufferedInputStream(new FileInputStream(theFile));
+            byte[] buffer = new byte[1024];
+            bis.skip(seq-1);
+            while(bis.read(buffer) != -1){
+                UDPPacket packetTemp = buildPacket(seq, ack,fin, buffer);
+                fenetre.put(seq, packetTemp);//On ajoute à la liste
+                this.setSeq(this.getSeq() + buffer.length);
+                
+            }            
+            
+
+        } catch (FileNotFoundException ex) {
+            java.util.logging.Logger.getLogger(transmissionHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            java.util.logging.Logger.getLogger(transmissionHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }        
+    private void sendPacket(UDPPacket udpPacket) {
+        try {
+               
+                logger.debug(udpPacket.toString());
+                byte[] packetData = Marshallizer.marshallize(udpPacket);
+                DatagramPacket datagram = new DatagramPacket(packetData,
+                                packetData.length, 
+                                udpPacket.getDestination(),
+                                udpPacket.getDestinationPort());
+                connectionSocket.send(datagram); // émission non-bloquante
+        } catch (SocketException e) {
+                System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+                System.out.println("IO: " + e.getMessage());
+        }
+    }
+    
+     private UDPPacket buildPacket(int seq, int ack, int fin, byte[] data) {
+        
+                UDPPacket packet = new UDPPacket(connectionPacket.getType(),connectionPacket.getDestination(),connectionPacket.getDestinationPort());
+                packet.setData(data);
+                packet.setSeq(seq);
+                packet.setSeq(ack);
+                packet.setFin(fin);
+                logger.debug(packet.toString());
+                return packet;
+     }
+    
+     public UDPPacket getConnectionPacket() {
+        return connectionPacket;
+    }
+
+    public void setConnectionPacket(UDPPacket udpPacket) {
+        this.connectionPacket = udpPacket;
+    }
+    
+    public void start() {		
+        try {
+                connectionSocket = new DatagramSocket(); // port convenu avec les clients
+                
+                logger.info("server start on port " + String.valueOf(connectionSocket.getPort()));
+                
+                boolean run = true; 
+                byte[] buffer = new byte[1500];
+                DatagramPacket datagram = new DatagramPacket(buffer, buffer.length);
+                Timer timer = new Timer(); //Timer pour les timeouts
+
+                //Création du paquet pour la confirmation de connexion
+                this.setSeq(1);
+                UDPPacket confirmConnectionPacket = buildPacket(seq,ack,fin,new byte[1024]);
+                
+                //Envoi d'un paquet avec un seq 1. 
+                timer.scheduleAtFixedRate(new TimerTask() {
+                    @Override
+                    public void run() {
+                      sendPacket(confirmConnectionPacket);
+                    }
+                  }, 0, 10000);
+                
+                //En attente du paquet de notre client avec seq1, ack1. Pour terminé le handshake.
+                connectionSocket.receive(datagram); // reception bloquante
+                
+                logger.info("Connection accepted by the client");
+                
+                //Arret du timer
+                timer.cancel();
+               
+                //We can start to send data to the client
+                do  {                   
+                    
+                    prepWindow();
+                    connectionSocket.receive(datagram); // reception bloquante
+                    
+                    logger.info("an ack was received");
+                    
+                    					
+                }while (run);
+        } catch (SocketException e) {
+                System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+                System.out.println("IO: " + e.getMessage());
+        }
+        finally {
+                logger.info("end of transmission");
+                stop();
+        }
+	}
+    public void stop(){
+        Thread.currentThread().interrupt();
+        return;
+    }
+    @Override
+	public void run() {
+		start();	
+	}
 }
