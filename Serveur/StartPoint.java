f2cff3e7c5a2b571a9294d8200747a1df11b2735
vince8lte <v.quernel@gmail.com>
2016-02-08 15:49:53 -0500

    Update StartPoint.java

--- Serveur/StartPoint.java
+++ Serveur/StartPoint.java
@@ -6,7 +6,7 @@ public class StartPoint {
 	public static void main(String[] args) {	
 		
 		//Create match service
-		int port = 6780;
+		int port = 55557;
 		int threadPoolSize = 4;	
 		Thread udpServer = new Thread( new  UDPServer(port,threadPoolSize));
 		udpServer.start();	
